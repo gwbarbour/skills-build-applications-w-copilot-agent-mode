4a78e2071ffea2374f1df95211aaf82836f7e017
gwbarbour <gwbarbour@gmail.com>
2026-04-21 05:51:01 +0000

    Add URL routing configuration with RedirectView for API endpoint

---
+++ from django.views.py
@@ -0,0 +1,8 @@
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='api/', permanent=False)),
+    path('admin/', admin.site.urls),
+    path('api/', views.api_root, name='api-root'),
+    path('api/', include(router.urls)),
+]
